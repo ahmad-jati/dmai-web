5ab45d66cdb7bae7cba9aa6de4e537dd6989e58c
Artia Aryatama <artyaaryatama21@gmail.com>
2026-07-03 23:15:53 +0800

    chore: test behavior scroll

--- app/(protected)/template.tsx
+++ app/(protected)/template.tsx
@@ -3,9 +3,9 @@
 import { useEffect } from "react";
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }, []);
+  // useEffect(() => {
+  //   window.scrollTo({ top: 0, behavior: 'smooth' });
+  // }, []);
 
   return <>{children}</>;
 }
