f1ea2825fc2106b2ba03307d18e419e4117a02cb
Artia Aryatama <artyaaryatama21@gmail.com>
2026-07-05 11:57:18 +0800

    chore: revert layout

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -3,7 +3,6 @@ import { ProtectedNavbar } from "@/components/navbars/protected-navbar";
 import { DummyPlaceholderNavbar } from "@/components/navbars/dummy-placeholder-navbar";
 import { AuthStateListener } from "@/components/auth-state-listener";
 import { PresenceTracker } from "@/components/presence-tracker";
-import { ScrollbarOffsetWatcher } from "@/components/scrollbar-offset-watcher";
 
 export default function ProtectedLayout({
   children,
@@ -12,13 +11,14 @@ export default function ProtectedLayout({
 }) {
   return (
     <div className="flex flex-col flex-1 relative">
-      <ScrollbarOffsetWatcher />
-      <AuthStateListener />
+      <AuthStateListener/>
       <PresenceTracker />
-      <ProtectedNavbar />
+      <ProtectedNavbar/>
       <div className="flex flex-col items-center md:px-16 px-6 gap-8 lg:max-w-7xl mx-auto w-full flex-1">
-        <DummyPlaceholderNavbar />
-        <div className="w-full">{children}</div>
+      <DummyPlaceholderNavbar/>
+        <div className="w-full">
+          {children}
+        </div>
         <Footer />
       </div>
     </div>
