5edc71092f05568ef8249aeefc7f73ab87f9fb58
Artia Aryatama <artyaaryatama21@gmail.com>
2026-07-11 19:39:14 +0800

    chore: change not found image

--- components/section/not-found/not-found-content.tsx
+++ components/section/not-found/not-found-content.tsx
@@ -14,9 +14,9 @@ export function NotFoundContent({
 }) {
   return (
     <div className="flex w-full">
-      <Section className="flex lg:flex-row-reverse flex-col-reverse lg:items-center justify-center items-end lg:gap-8 gap-0 bg-white dark:bg-card">
-        <div className="w-full lg:w-fit flex flex-col gap-2.5 items-start text-foreground md:px-0 px-2 2md:mt-0">
-          <EyesIcon className="text-foreground w-10 h-10" weight="fill" />
+      <Section className="flex flex-col-reverse items-center justify-center gap-4 bg-white dark:bg-card">
+        <div className="w-full lg:w-fit flex flex-col gap-2.5 items-center text-foreground md:px-0 px-2 2md:mt-0">
+          {/* <EyesIcon className="text-foreground w-10 h-10" weight="fill" /> */}
           <h2 className="sm:text-h2 text-xl font-semibold -mt-1.5">Oops!</h2>
           <p className="xs:text-p/5 text-sm/4 font-medium text-pretty">
             Halaman yang kamu cari tidak ditemukan
@@ -25,7 +25,8 @@ export function NotFoundContent({
           <Link href={backHref as Route} className="flex gap-2 items-center">
             <Button
               variant={"default"}
-              className="[&_svg]:size-4 xs:text-p/5 text-xs/4 rounded-xl hover:bg-background/80 dark:bg-primary"
+              size={'sm'}
+              className="[&_svg]:size-4 xs:text-p/5 text-xs/4 rounded-md bg-background hover:bg-background/80 dark:bg-primary px-4!"
             >
               <ArrowLeftIcon />
               Kembali ke Beranda
@@ -33,15 +34,15 @@ export function NotFoundContent({
           </Link>
         </div>
 
-        <div className="xl:w-50 lg:w-56 lg:h-106 2md:w-50 2xs:w-60 w-50 h-fit xs:px-0 p-4">
+        <div className=" 2xs:w-60 w-40 h-fit xs:px-0 p-4 aspect-square  rounded-xl">
           <Image
-            src={"/tropicaline/compress/page-not-found.png"}
+            src={"open-doodles/ReadingDoodle.svg"}
             alt=""
             width={2000}
             height={2000}
             unoptimized
             priority
-            className="w-full h-full object-contain"
+            className="w-full h-full object-cover"
           />
         </div>
       </Section>
